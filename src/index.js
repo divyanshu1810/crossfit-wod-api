f42c54fba9c39d27fc8fa5230a52504fcffa37b5
Divyanshu Yadav <divyanshu.1810@gmaiil.com>
2023-03-05 13:01:50 +0530

    :sparkles: Added Architecture

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-// In src/index.js 
-const express = require("express"); 
+// In src/index.js
+const express = require("express");
+// *** ADD ***
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
-const app = express(); 
-const PORT = process.env.PORT || 3000; 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// For testing purposes 
-app.get("/", (req, res) => { 
-    res.send("<h2>It's Working!</h2>"); 
-}); 
+// *** ADD ***
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
-app.listen(PORT, () => { 
-    console.log(`API is listening on port ${PORT}`); 
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
 });
